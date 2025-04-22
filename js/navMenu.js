b2a75c1ae292e290cd8b8413e96c1440cc28b318
pauloserverpt <pauloserverpt@gmail.com>
2025-04-22 14:15:18 +0000

    Hamburger melhorado

--- js/navMenu.js
+++ js/navMenu.js
@@ -1,6 +1,17 @@
 // Seleciona o ícone do hamburger e o menu
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('nav ul');
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.appendChild(overlay);
+
+
+// Função para fechar o menu e remover o overlay
+function closeMenu() {
+    menu.classList.remove('show');
+    hamburger.classList.remove('active');
+    overlay.style.display = 'none';
+}
 
 // Adiciona um ouvinte de evento ao ícone do hamburger
 hamburger.addEventListener('click', () => {
@@ -8,4 +19,14 @@ hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
     // Alterna a classe 'active' no ícone do hamburger para animação
     hamburger.classList.toggle('active');
+
+    //mostra o overlay quando o menu é aberto
+    if (menu.classList.contains('show')) {
+        overlay.style.display = 'block';
+    } else {
+        overlay.style.display = 'none';
+    }
+});
+menu.addEventListener('click', closeMenu)
+
 });
