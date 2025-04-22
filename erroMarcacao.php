c982a4bb65187b3d8f8c9cc7deb0e6a6d48dc21a
pauloserverpt <pauloserverpt@gmail.com>
2025-04-22 08:48:32 +0000

    mais

--- erroMarcacao.php
+++ erroMarcacao.php
@@ -16,25 +16,25 @@ php
     <?php include('./includes/header.php'); ?> <!-- Inclui o cabeçalho da página -->
 </head>
 <body>
-    <main>
-        <section class="appointment-section">
-            <div class="error-message-container">
-                <h1>Ocorreu um Erro</h1> <!-- Título principal da página de erro -->
-                <?php
-                    // Verifica se o parâmetro 'error' está definido na URL.
-                    if (isset($_GET['error'])) {
-                        // Recupera a mensagem de erro e garante a sua segurança com htmlspecialchars().
-                        $errorMessage = htmlspecialchars($_GET['error']);
-                        echo "<p class='error-message'>$errorMessage</p>"; // Exibe a mensagem de erro
-                    } else {
-                        // Se não houver mensagem de erro, exibe uma mensagem padrão.
-                        echo "<p class='error-message'>Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.</p>";
-                    }
-                ?>
-                <p>Por favor, tente fazer a sua marcação novamente.</p>
-                <a href="marcacoes.php" class="btn-confirm">Voltar para Marcações</a>
-            </div>
-        </section>
+    <main> 
+        <section class="appointment-section"> 
+            <div class="confirmation-message"> 
+                <h1>Ocorreu um Erro</h1> 
+                <?php 
+                // Verifica se o parâmetro 'error' está definido na URL. 
+                if (isset($_GET['error'])) { 
+                    // Recupera a mensagem de erro e garante a sua segurança com htmlspecialchars(). 
+                    $errorMessage = htmlspecialchars($_GET['error']); 
+                    echo "<p class='error-message'>$errorMessage</p>"; // Exibe a mensagem de erro 
+                } else { 
+                    // Se não houver mensagem de erro, exibe uma mensagem padrão. 
+                    echo "<p class='error-message'>Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.</p>"; 
+                } 
+                ?> 
+                <p>Por favor, tente fazer a sua marcação novamente.</p> 
+                <a href="marcacoes.php" class="btn-confirm">Voltar para Marcações</a> 
+            </div> 
+        </section> 
     </main>
     <footer>
         <?php include('./includes/footer.php'); ?> <!-- Inclui o footer da página -->
