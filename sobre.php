5c984502e6f72f1756e863d86130cf6ee2b66487
PauloSilva <1221528@isep.ipp.pt>
2025-04-23 10:09:28 +0100

    Att Sobre Nos + Icons

--- sobre.php
+++ sobre.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre</title>
     <link rel="stylesheet" href="./css/sobre.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <main>
-    <!-- Seção de resumo da barbearia -->
     <section class="about">
         <div class="text">
             <div class="section-title">
-                <h1>Sobre Nós</h1>
+                <h1><i class="fas fa-info-circle"></i> Sobre Nós</h1>
             </div>
             <div class="section-content">
                 <p>A Oficina do Cabelo, localizada em Ermesinde, é mais do que uma barbearia: é um espaço onde estilo e tradição se encontram. Desde 2019, oferecemos cortes personalizados, cuidados com a barba e um ambiente acolhedor para os nossos clientes. Venha visitar-nos e experimente o verdadeiro espírito da barbearia moderna!</p>
@@ -21,36 +21,43 @@
         </div>
         <div class="image">
             <img src="./img/Space.png" alt="Imagem da Barbearia">
+            <p class="image-caption">Nosso espaço acolhedor em Ermesinde</p>
         </div>
     </section>
-    <!-- Seção dos barbeiros -->
     <section class="barbers">
         <div class="section-title">
-            <h2>Conheça os Nossos Barbeiros</h2>
+            <h2><i class="fas fa-cut"></i> Conheça os Nossos Barbeiros</h2>
         </div>
         <div class="barbers-grid">
-            <!-- Barbeiro 1 -->
             <div class="barber">
                 <a href="#" class="barber-link" data-barber="barber1">
-                    <img src="./img/BBarber.png" alt="Barbeiro 1">
+                    <img src="./img/BBarber.png" alt="Barbeiro Bruno Martins em ação na Oficina do Cabelo">
                 </a>
-                <h3>Bruno Martins</h3>
+                <div class="barber-name">
+                    <h3>Bruno Martins</h3>
+                </div>
             </div>
-            <!-- Barbeiro 2 -->
             <div class="barber">
                 <a href="#" class="barber-link" data-barber="barber2">
-                    <img src="./img/HBarber.png" alt="Barbeiro 2">
+                    <img src="./img/HBarber.png" alt="Barbeiro Hugo Alves em ação na Oficina do Cabelo">
                 </a>
-                <h3>Hugo Alves</h3>
+                <div class="barber-name">
+                    <h3>Hugo Alves</h3>
+                </div>
             </div>
         </div>
     </section>
-    <!-- Modal de sobreposição -->
     <div id="barber-modal" class="modal">
         <div class="modal-content">
-            <span class="close">&times;</span>
+            <span class="close">×</span>
             <div class="modal-text">
-                <!-- Conteúdo do resumo será inserido aqui dinamicamente -->
+                <h3 class="modal-title"></h3>
+                <div class="modal-image-container">
+                    <img class="modal-image" src="" alt="Imagem do Barbeiro">
+                    <button class="instagram-button"><i class="fab fa-instagram"></i> Instagram</button>
+                </div>
+                <p class="modal-description"></p>
+                <a href="./marcacoes.php" class="modal-button"><i class="fas fa-calendar-alt"></i> Agendar com Este Barbeiro</a>
             </div>
         </div>
     </div>
