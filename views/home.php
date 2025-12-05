ec404ad48eb45f929d704d46ea91aec83efcc9b0
PauloSilva <1221528@isep.ipp.pt>
2025-12-05 19:30:38 +0000

    Update Services section styling and fix dark mode text visibility

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@ $path_prefix = './';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oficina do Cabelo - Barbearia Clássica</title>
-    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/index.css?v=<?= time() ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
